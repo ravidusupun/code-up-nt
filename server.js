830f47aad043bb1db175e9f02c19adff71c6f7ec
ravidusupun <supunravidu96@gmail.com>
2025-04-08 15:52:23 +0930

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    if (req.url === '/') {
+        res.end('Hello World!\n');
+    } else if (req.url === '/about') {
+        res.end('Project Name: AquaLeanyer Description:server of project in Node.js');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found\n');
+    }
+}).listen(3000);
+
+console.log('Server running on port 3000');
+
+// netstat -ano | findstr :3000
+// node server.js
